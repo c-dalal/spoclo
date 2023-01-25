c7ee4ecd2cc39bb9a09da57f709c26aa8610518c
Chandni Dalal <chandnidalal2@gmail.com>
2023-01-25 12:54:22 +0530

    Added my project

---
+++ src/spotify.js
@@ -0,0 +1,16 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "770aed15853244c1a8c267ae7a32828f";
+const redirectUri = "http://localhost:3000/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
